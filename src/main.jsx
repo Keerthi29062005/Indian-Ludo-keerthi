7127429a1ba21b8b67ae23874bc53d08ef099a11
Heramb <hs22csb0f19@student.nitw.ac.in>
2024-05-17 21:06:14 +0530

    Modified the CSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Board from './components/Board.jsx'
-import './index.css'
+import './index.css';
+import './components/main.css';
+
+const RootComponent = () => (
+  <div className="Entire">
+    <Board className="Box"/>
+    <App className="Box"/>
+  </div>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Board />
-    <App />
+    <RootComponent />
   </React.StrictMode>,
-)
+);
