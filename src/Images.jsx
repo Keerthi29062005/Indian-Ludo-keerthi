300eeb60880ee5210d579d5277c1f749ac13e1de
Heramb Rallapally <154792879+Heramb-Rallapally@users.noreply.github.com>
2024-05-17 14:25:40 +0530

    adding heramb src folders

---
+++ src/Images.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import gavullu from '../assests/gavullu.jpg';
+function Images_front() {
+    return (
+        <div>
+            <img src={gavullu} alt="png" width="80" height="100"></img>
+        </div>
+    );
+}
+export default Images_front;
